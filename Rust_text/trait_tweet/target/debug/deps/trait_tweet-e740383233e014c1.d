9ddcdbbbb4f173fc82889b544b3c0f62d507de12
Hidenori24 <49014675+Hidenori24@users.noreply.github.com>
2024-02-10 22:09:40 +0900

    add traint and dyn project

---
+++ Rust_text/trait_tweet/target/debug/deps/trait_tweet-e740383233e014c1.d
@@ -0,0 +1,5 @@
+/Users/matsumoto/work/Rust_dev/Rust_text/trait_tweet/target/debug/deps/trait_tweet-e740383233e014c1: src/main.rs
+
+/Users/matsumoto/work/Rust_dev/Rust_text/trait_tweet/target/debug/deps/trait_tweet-e740383233e014c1.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_text/trait_tweet/target/debug/trait_tweet.d
@@ -0,0 +1,1 @@
+/Users/matsumoto/work/Rust_dev/Rust_text/trait_tweet/target/debug/trait_tweet: /Users/matsumoto/work/Rust_dev/Rust_text/trait_tweet/src/main.rs
