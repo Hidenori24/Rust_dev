e128ee7b80f742ecc14d04191c6c350e8e877d82
Hidenori24 <49014675+Hidenori24@users.noreply.github.com>
2024-02-19 01:04:41 +0900

    update connect message (but cant compile, I added l.16 .clone() )

--- Rust_text/message_passing/src/main.rs
+++ Rust_text/message_passing/src/main.rs
@@ -2,13 +2,34 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let handle = thread::spawn(move || {
-        let data = rx.recv().unwrap();
-        println!("{}", data);
-    });
+    let mut handles = Vec::new();
+    let mut data = vec![1; 10];
+    let mut snd_channels = Vec::new();
+    let mut rcv_channels = Vec::new();
 
-    let _ = tx.send("Hello World!");
+    for _ in 0..10 {
+        // main から各スレッドへのチャンネル
+        let (snd_tx, snd_rx) = mpsc::channel();
+        // 各スレッドからmainへのチャンネル
+        let (rcv_tx, rcv_rx) = mpsc::channel();
+        snd_channels.push(snd_tx);
+        rcv_channels.push(rcv_tx.clone());
 
-    let _ = handle.join();
+        handles.push(thread::spawn(move || {
+            let mut data = snd_rx.recv().unwrap();
+            data += 1;
+            let _ = rcv_tx.send(data);
+        }));
+    }
+
+    //各スレッドにdataの値の送信
+    for x in 0..10 {
+        let _ = snd_channels[x].send(data[x]);
+    }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+
+    dbg!(data);
 }
