a60eab8597b025f8e332b2e3ee1597b78c4e6ab4
Hidenori24 <49014675+Hidenori24@users.noreply.github.com>
2024-02-24 01:02:23 +0900

    エラーハンドリングを追加．

--- Rust_text_cp4/samplecli/src/bin/err_panic.rs
+++ Rust_text_cp4/samplecli/src/bin/err_panic.rs
@@ -1,15 +1,17 @@
-fn get_from_file() -> i32 {
+fn get_from_file() -> Result<i32, String> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
-    let ret = num_str
-        .trim()
-        .parse::<i32>()
-        .expect("failed to parse string to a number.");
-
-    ret * 2
+    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    num_str // 最初 num_str は"&str"型
+        .trim() // 文字列前後の空白文字を削除
+        .parse::<i32>() // '&str' を 'i32' に変換する．結果は Result<i32, ParseIntError>
+        .map(|t| t * 2) // parseの結果が Ok(t)の場合のみここを実行
+        .map_err(|e| e.to_string()) // parseの結果が Err(e)の場合のみeの文字列表現
 }
 
 fn main() {
-    println!("{}", get_from_file());
+    match get_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
 }
