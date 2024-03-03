d0c660d001ead7c393306628a553d30486428a33
Hidenori24 <49014675+Hidenori24@users.noreply.github.com>
2024-03-03 23:53:25 +0900

    update using for DataBase

---
+++ Rust_text_cp5/todo/target/debug/build/libsqlite3-sys-0af845cd433ea204/build_script_build-0af845cd433ea204.d
@@ -0,0 +1,5 @@
+/Users/matsumoto/work/Rust_dev/Rust_text_cp5/todo/target/debug/build/libsqlite3-sys-0af845cd433ea204/build_script_build-0af845cd433ea204: /Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsqlite3-sys-0.28.0/build.rs
+
+/Users/matsumoto/work/Rust_dev/Rust_text_cp5/todo/target/debug/build/libsqlite3-sys-0af845cd433ea204/build_script_build-0af845cd433ea204.d: /Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsqlite3-sys-0.28.0/build.rs
+
+/Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libsqlite3-sys-0.28.0/build.rs:
---
+++ Rust_text_cp5/todo/target/debug/deps/vcpkg-6db4bad9ac5c6188.d
@@ -0,0 +1,7 @@
+/Users/matsumoto/work/Rust_dev/Rust_text_cp5/todo/target/debug/deps/libvcpkg-6db4bad9ac5c6188.rmeta: /Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vcpkg-0.2.15/src/lib.rs
+
+/Users/matsumoto/work/Rust_dev/Rust_text_cp5/todo/target/debug/deps/libvcpkg-6db4bad9ac5c6188.rlib: /Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vcpkg-0.2.15/src/lib.rs
+
+/Users/matsumoto/work/Rust_dev/Rust_text_cp5/todo/target/debug/deps/vcpkg-6db4bad9ac5c6188.d: /Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vcpkg-0.2.15/src/lib.rs
+
+/Users/matsumoto/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vcpkg-0.2.15/src/lib.rs:
